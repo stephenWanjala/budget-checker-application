77cd6ed3c7f0173762058a2efb241fb7c3cd067f
wanjala stephen <stephenwanjala145@gmail.com>
2022-06-04 05:41:37 +0300

    clean up

--- app/src/main/java/com/wtech/budgetchecker/MainActivity.kt
+++ app/src/main/java/com/wtech/budgetchecker/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.wtech.budgetchecker
 
-import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -18,44 +16,44 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     private lateinit var transaction: Transaction
     private lateinit var deletedTransaction: Transaction
-    private lateinit var transactionList:List<Transaction>
-    private lateinit var oldTransactionList:List<Transaction>
-private lateinit var binding: ActivityMainBinding
-private lateinit var  myAdapter: TransactionAdapter
-private lateinit var mLayoutManager: RecyclerView.LayoutManager
+    private lateinit var transactionList: List<Transaction>
+    private lateinit var oldTransactionList: List<Transaction>
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var myAdapter: TransactionAdapter
+    private lateinit var mLayoutManager: RecyclerView.LayoutManager
 
-private lateinit var  dataBase: AppDataBase
+    private lateinit var dataBase: AppDataBase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        transactionList= arrayListOf()
-        myAdapter= TransactionAdapter(transactionList)
-        mLayoutManager=LinearLayoutManager(this@MainActivity)
-        dataBase= Room.databaseBuilder(
-            this@MainActivity,AppDataBase::class.java,"transactions"
+        transactionList = arrayListOf()
+        myAdapter = TransactionAdapter(transactionList)
+        mLayoutManager = LinearLayoutManager(this@MainActivity)
+        dataBase = Room.databaseBuilder(
+            this@MainActivity, AppDataBase::class.java, "transactions"
         ).build()
 
         binding.recyclerView.apply {
-            layoutManager=mLayoutManager
-            adapter=myAdapter
+            layoutManager = mLayoutManager
+            adapter = myAdapter
         }
         fetchAll()
 //removed as on create call on resume
         binding.floatingActionButton.setOnClickListener {
-            startActivity(Intent(this@MainActivity,AddTransaction::class.java))
+            startActivity(Intent(this@MainActivity, AddTransaction::class.java))
         }
 
 
 //        swipeTo delete
-        val itemTouchHelper=object :ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.RIGHT){
+        val itemTouchHelper = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
             override fun onMove(
                 recyclerView: RecyclerView,
                 viewHolder: RecyclerView.ViewHolder,
                 target: RecyclerView.ViewHolder,
             ): Boolean {
-               return false
+                return false
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
@@ -65,7 +63,7 @@ private lateinit var  dataBase: AppDataBase
         }
 //instanciate  the ontouchHelper
 
-        val swipeHelper=ItemTouchHelper(itemTouchHelper)
+        val swipeHelper = ItemTouchHelper(itemTouchHelper)
 
 //Attach to the  recycler view
         swipeHelper.attachToRecyclerView(binding.recyclerView)
@@ -73,11 +71,11 @@ private lateinit var  dataBase: AppDataBase
     }
 
 
-    private fun fetchAll(){
+    private fun fetchAll() {
         GlobalScope.launch {
 
 //            dataBase.transactionDao().insertAll(Transaction(0,"Ice cream",-80.00,"ate It"))
-            transactionList=dataBase.transactionDao().getAll()
+            transactionList = dataBase.transactionDao().getAll()
 
             runOnUiThread {
                 updateUi()
@@ -87,34 +85,31 @@ private lateinit var  dataBase: AppDataBase
     }
 
 
-
-
-
-
-    private fun updateUi(){
+    private fun updateUi() {
         val totalAmount: Double = transactionList.sumOf { it.amount }
-        val budget= transactionList.filter { it.amount > 0 }.sumOf { it.amount }
-        val expense=totalAmount-budget
+        val budget = transactionList.filter { it.amount > 0 }.sumOf { it.amount }
+        val expense = totalAmount - budget
 
         binding.totalAmount.text = "$%.3f".format(totalAmount)
         binding.budgetAmount.text = "$%.2f".format(budget)
         binding.balanceAmount.text = "$%.2f".format(expense)
     }
-    private fun  showSnackBar(){
-        val view=binding.coordinatorLayout
-        val snackbar=Snackbar.make(view,"Transaction Deleted!",Snackbar.LENGTH_LONG)
-        snackbar.setAction("Undo"){
+
+    private fun showSnackBar() {
+        val view = binding.coordinatorLayout
+        val snackbar = Snackbar.make(view, "Transaction Deleted!", Snackbar.LENGTH_LONG)
+        snackbar.setAction("Undo") {
             undoDeleteTransaction()
         }
-            .setActionTextColor(ContextCompat.getColor(this@MainActivity,R.color.red))
-            .setTextColor(ContextCompat.getColor(this@MainActivity,R.color.white))
+            .setActionTextColor(ContextCompat.getColor(this@MainActivity, R.color.red))
+            .setTextColor(ContextCompat.getColor(this@MainActivity, R.color.white))
             .show()
     }
 
     private fun undoDeleteTransaction() {
         GlobalScope.launch {
             dataBase.transactionDao().insertAll(deletedTransaction)
-            transactionList=oldTransactionList
+            transactionList = oldTransactionList
 
             runOnUiThread {
                 myAdapter.setTransactionData(transactionList)
@@ -124,13 +119,13 @@ private lateinit var  dataBase: AppDataBase
         }
     }
 
-    fun deleteTransaction(transaction: Transaction){
-        deletedTransaction=transaction
-        oldTransactionList=transactionList
+    fun deleteTransaction(transaction: Transaction) {
+        deletedTransaction = transaction
+        oldTransactionList = transactionList
         GlobalScope.launch {
             dataBase.transactionDao().delete(transaction)
-            transactionList=transactionList.filter { transact ->
-                transact.id!=transaction.id
+            transactionList = transactionList.filter { transact ->
+                transact.id != transaction.id
             }
             runOnUiThread {
                 myAdapter.setTransactionData(transactionList)
@@ -139,10 +134,6 @@ private lateinit var  dataBase: AppDataBase
             }
         }
 
-
-    }
-    override fun onRestart() {
-        super.onRestart()
 
     }
 
